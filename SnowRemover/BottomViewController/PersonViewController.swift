0a63d3a669daca25ff774c121629cc05e67df03f
Simarmultani <Simarjitsingh32@gmail.com>
2022-04-20 13:28:46 -0400

    person view and person detail view

--- SnowRemover/BottomViewController/PersonViewController.swift
+++ SnowRemover/BottomViewController/PersonViewController.swift
@@ -11,15 +11,32 @@ import FirebaseFirestore
 import CloudKit
 
 
-class PersonViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class PersonViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var personCollection: UICollectionView!
     var products : [ProductModel] = []
+    var userUID : String = ""
+    var copyProducts : [ProductModel] = []
+    var favProductIds : [String] = []
+    
+    @IBOutlet weak var dropDownMenu: UIImageView!
+    
+    @IBOutlet weak var selectMenu: UIButton!
+    
+    @IBOutlet var menuItems: [UIButton]!
+    
+    @IBOutlet weak var searchPerson: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        searchPerson.delegate = self
+        if Auth.auth().currentUser != nil {
+            userUID = Auth.auth().currentUser?.uid ?? ""
+            getFavourite()
+        }
         
         let layout = UICollectionViewFlowLayout()
         
@@ -33,6 +50,19 @@ class PersonViewController: UIViewController, UICollectionViewDelegate, UICollec
         loadData();
         personCollection.delegate = self
         personCollection.dataSource = self
+        
+        selectMenu.layer.cornerRadius = selectMenu.frame.height / 2
+        selectMenu.isHidden = true
+        selectMenu.alpha = 0
+        menuItems.forEach { (btn) in
+            btn.layer.cornerRadius = btn.frame.height / 2
+            btn.isHidden = true
+            btn.alpha = 0
+        }
+        
+        let menuGasture = UITapGestureRecognizer(target: self, action: #selector(openDropDownMenu(tapGestureRecognizer:)))
+        dropDownMenu.isUserInteractionEnabled = true
+        dropDownMenu.addGestureRecognizer(menuGasture)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -45,7 +75,6 @@ class PersonViewController: UIViewController, UICollectionViewDelegate, UICollec
         secondViewController.modalPresentationStyle = .fullScreen
         secondViewController.personId = products[indexPath.row].id
         self.present(secondViewController, animated:true, completion:nil)
-        print("on click")
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -87,15 +116,100 @@ class PersonViewController: UIViewController, UICollectionViewDelegate, UICollec
                     let docId = document.documentID
                     let nameValue: String = data["name"] as? String ?? ""
                     let priceValue: String = data["Price"] as? String ?? ""
+                    let priceDouble : Double = Double(priceValue) ?? 0
                     let imageUrlValue: String = data["imageurl"] as? String ?? ""
+                    let count : Int = 1
                     let type : String = "person"
-                    
-                    products.append(ProductModel(id: docId, name: nameValue, price: priceValue, imageurl: imageUrlValue, type: type))
+                    let isArchive : Bool = data["archive"] as? Bool ?? false
+                    if(!isArchive){
+                        products.append(ProductModel(id: docId, name: nameValue, price: priceDouble, imageurl: imageUrlValue, type: type, count: count))
+                    }
                 }
+                self.copyProducts.append(contentsOf: products)
                 self.personCollection.reloadData()
             }
         }
         
+    }
+    
+    func getFavourite(){
+        let db = Firestore.firestore()
+        db.collection("users").document(userUID).collection("favorite").getDocuments(){ [self] (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let id : String = document.documentID
+                    print(id)
+                    favProductIds.append(id)
+                }
+            }
+        }
+    }
+    
+    @IBAction func selectMenuAction(_ sender: Any) {
+        menuItems.forEach { (btn) in
+            UIView.animate(withDuration: 0.3) {
+                btn.isHidden = !btn.isHidden
+                btn.alpha = btn.alpha == 0 ? 1 : 0
+                btn.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @IBAction func selectMenuItemAction(_ sender: UIButton) {
+        let value : String = sender.titleLabel?.text ?? ""
+        products.removeAll()
+        if(value == "Available"){
+            for product in copyProducts {
+                if(product.count > 0){
+                    products.append(product)
+                }
+            }
+        }
+        if(value == "Low To High"){
+            products.append(contentsOf: copyProducts.sorted(by: { $0.price < $1.price }))
+        }
+        if(value == "High To Low"){
+            products.append(contentsOf: copyProducts.sorted(by: { $0.price > $1.price }))
+        }
+        if(value == "Favorite"){
+            for product in copyProducts {
+                if(favProductIds.contains(product.id)){
+                    self.products.append(product)
+                }
+            }
+        }
+        self.personCollection.reloadData()
+    }
+    
+    @objc func openDropDownMenu(tapGestureRecognizer: UITapGestureRecognizer){
+        UIView.animate(withDuration: 0.3) {
+            self.selectMenu.isHidden = !self.selectMenu.isHidden
+            self.selectMenu.alpha = self.selectMenu.alpha == 0 ? 1 : 0
+            self.selectMenu.layoutIfNeeded()
+            self.menuItems.forEach { (btn) in
+                UIView.animate(withDuration: 0.3) {
+                    btn.isHidden = true
+                    btn.alpha = 0
+                    btn.layoutIfNeeded()
+                }
+            }
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        products.removeAll()
+        if(searchText.isEmpty){
+            products.append(contentsOf: copyProducts)
+        }else{
+            for product in copyProducts {
+                if(product.name.uppercased().contains(searchText.uppercased())){
+                    products.append(product)
+                }
+            }
+        }
+        self.personCollection.reloadData()
     }
 
 }
--- SnowRemover/ViewControllers/PersondetailViewController.swift
+++ SnowRemover/ViewControllers/PersondetailViewController.swift
@@ -11,14 +11,34 @@ import Firebase
 class PersondetailViewController: UIViewController {
 
     var personId = String()
+    var favIcon : [UIImage] = [
+        UIImage(systemName: "heart")!,
+        UIImage(systemName: "heart.fill")!]
+
+    var isFavourite : Bool = false
+    
+    @IBOutlet weak var personHours: UILabel!
+    var userUID : String = ""
     
     @IBOutlet weak var personName: UILabel!
     @IBOutlet weak var personDescription: UILabel!
     @IBOutlet weak var personPrice: UILabel!
     @IBOutlet weak var personAge: UILabel!
-    
+    @IBOutlet weak var favouriteImage: UIImageView!
     @IBOutlet weak var personImage: UIImageView!
     
+    var favProductIds : [String] = []
+    let db = Firestore.firestore()
+    var hour : Int = 1
+    
+    var name : String = ""
+    var image : String = ""
+    var price : String = ""
+    var type : String = "person"
+    var hours : String = "1"
+    var quantity : String = "1"
+    var cartid : [String] = []
+    var cartQuntity : [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +46,32 @@ class PersondetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         LoadData()
+        if Auth.auth().currentUser != nil {
+            userUID = Auth.auth().currentUser?.uid ?? ""
+            print(userUID)
+            getFavourite()
+            
+            
+        }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ProductDetailViewController.AddToFavourite(gesture:)))
+        favouriteImage.addGestureRecognizer(tapGesture)
+        favouriteImage.isUserInteractionEnabled = true
     }
+    
+    @IBAction func removeHours(_ sender: Any) {
+        if hour > 1{
+            hour = hour - 1
+            let qnt = String(hour)
+            personHours.text = qnt
+        }
+    }
+    
+    @IBAction func addHours(_ sender: Any) {
+        hour = hour + 1
+        let qnt = String(hour)
+        personHours.text = qnt
+    }
+    
     
     func LoadData(){
         let db = Firestore.firestore()
@@ -36,14 +81,14 @@ class PersondetailViewController: UIViewController {
                 // ERROR
             }
             else {
-                let name = snapshot?.get("name") as? String ?? "productname"
+                self.name = snapshot?.get("name") as? String ?? "productname"
                 let description = snapshot?.get("description") as? String ?? "Description"
-                let price = snapshot?.get("Price") as? String ?? "300"
-                let imageUrl = snapshot?.get("imageurl") as? String ?? "Url"
+                self.price = snapshot?.get("Price") as? String ?? "300"
+                self.image = snapshot?.get("imageurl") as? String ?? "Url"
                 let age = snapshot?.get("age") as? Int ?? 20
                 let ageVlue = String(age)
                 
-                let gsReference = Storage.storage().reference(forURL:  "gs://snowremovalapp-ac3d9.appspot.com/personimages/"+imageUrl)
+                let gsReference = Storage.storage().reference(forURL:  "gs://snowremovalapp-ac3d9.appspot.com/personimages/"+self.image)
                 gsReference.downloadURL(completion: { (url, error) in
                     if ((url?.isFileURL) != nil){
                         ImageService.downloadImage(withURL: url!) { image in
@@ -52,9 +97,9 @@ class PersondetailViewController: UIViewController {
                     }
                 })
                 
-                self.personName.text = name
+                self.personName.text = self.name
                 self.personDescription.text = description
-                self.personPrice.text = "$"+price
+                self.personPrice.text = "$"+self.price
                 self.personAge.text = ageVlue
                 
             }
@@ -63,6 +108,95 @@ class PersondetailViewController: UIViewController {
     
     
     @IBAction func AddToCart(_ sender: Any) {
+        if Auth.auth().currentUser != nil {
+            userUID = Auth.auth().currentUser?.uid ?? ""
+            
+            quantity = personHours.text ?? "1"
+            
+            let hourINT = Int(quantity)
+            let quantityINT = Int(hours)
+            let priceDouble  = Double(price)
+            
+            let cartData = ["id" : personId, "image" : "personimages/"+image, "name" : name, "price" : priceDouble, "quantity" : quantityINT, "type" : type, "hours" : hourINT] as [String : Any]
+            
+            db.collection("users").document(userUID).collection("cart").document(personId).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    print("document exist")
+                    var cartQuantity : Int = document.get("hours") as? Int ?? 0
+                    cartQuantity = cartQuantity + (hourINT ?? 0)
+                    
+                    document.reference.updateData([
+                        "hours": cartQuantity
+                                ])
+                    self.showToast("Cart Upadted Sucessfully!")
+                    
+                } else {
+                    print("document not exist")
+                    self.db.collection("users").document(self.userUID).collection("cart").document(self.personId).setData(cartData){error in
+                    if error != nil {
+                        
+                    }else{
+                        self.showToast("service added successfully")
+                    }
+                    }
+                }
+            }
+        }else{
+            self.showToast("You need to login first")
+        }
+    }
+    
+    func getFavourite(){
+        
+        db.collection("users").document(userUID).collection("favorite").getDocuments(){ [self] (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    let id : String = data["id"] as? String ?? ""
+                    if(id == personId){
+                        isFavourite = true
+                        favouriteImage.image = favIcon[1]
+                    }
+                    favProductIds.append(id)
+                }
+            }
+        }
+    }
+        
+    
+    @objc func AddToFavourite(gesture: UIGestureRecognizer) {
+        if Auth.auth().currentUser != nil {
+            userUID = Auth.auth().currentUser?.uid ?? ""
+        if (gesture.view as? UIImageView) != nil {
+                if(isFavourite){
+                    db.collection("users").document(userUID).collection("favorite").document(personId).delete(){error in
+                        if error != nil {
+                            
+                        }else{
+                            self.isFavourite = !self.isFavourite
+                            self.showToast("services removed as favorite!")
+                            self.favouriteImage.image = self.favIcon[0]
+                        }
+                    }
+                }else{
+                    let data = ["id" : personId, "type" : "person"]
+                    
+                    db.collection("users").document(userUID).collection("favorite").document(personId).setData(data){error in
+                    if error != nil {
+                       
+                    }else{
+                        self.isFavourite = !self.isFavourite
+                        self.showToast("services added as favorite!")
+                        self.favouriteImage.image = self.favIcon[1]
+                    }
+                }
+            }
+            }
+        }else{
+            self.showToast("You need to login first")
+        }
     }
     
     @IBAction func GotoHomeScreen(_ sender: Any) {
